53c451021c2df21e17facd93a7e9ba94e60cb187
FRANZ WILLAN MARTINEZ CHOQUE <visualsoft.s.a@gmail.com>
2019-07-09 23:19:20 -0400

    Primera Version del sistemas

---
+++ JALLPITASBAdmin/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace JALLPITASBAdmin.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
