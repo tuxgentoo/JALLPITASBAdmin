c7fa11ef05969c3ffb0f0f0907e7be393760377c
FRANZ WILLAN MARTINEZ CHOQUE <visualsoft.s.a@hotmail.com>
2020-01-15 22:51:52 -0400

    Registro de planillas

---
+++ JALLPITASBAdmin/Models/PlanillaRegistro.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JALLPITASBAdmin.Models
+{
+    public class PlanillaRegistro
+    {
+        public int PlanillaRegistroId { get; set; }
+        public string Nombre { get; set; }
+        public int Ci { get; set; }
+        public int Celular { get; set; }
+        public int TipoPersona { get; set; }
+        public int Departamento { get; set; }
+        public string TemaConsulta { get; set; }
+        public DateTime Fecha { get; set; }
+    }
+}
