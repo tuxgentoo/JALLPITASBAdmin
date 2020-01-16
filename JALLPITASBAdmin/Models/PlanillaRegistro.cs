e22728f5d0835c98c75ddccdbe31e348e19d18ff
FRANZ WILLAN MARTINEZ CHOQUE <visualsoft.s.a@gmail.com>
2020-01-16 00:51:17 -0400

    planilla

--- JALLPITASBAdmin/Models/PlanillaRegistro.cs
+++ JALLPITASBAdmin/Models/PlanillaRegistro.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -12,8 +13,11 @@ namespace JALLPITASBAdmin.Models
         public int Ci { get; set; }
         public int Celular { get; set; }
         public int TipoPersona { get; set; }
-        public int Departamento { get; set; }
-        public string TemaConsulta { get; set; }
+        [ForeignKey("Departamento")]
+        public int DepartamentoId { get; set; }
+        public virtual Departamento Departamento { get; set; }        
+        public string Proceso { get; set; }
+        public string Observacion { get; set; }
         public DateTime Fecha { get; set; }
     }
 }
